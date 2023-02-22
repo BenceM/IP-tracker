532061cd8c94555be345ac72954283e37b219785
Bence <m.bence211@gmail.com>
2023-02-22 20:28:24 +0100

    map height calculation

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
 "use strict";
 
+const mainDiv = document.querySelector(".main");
+const mapDiv = document.querySelector(".map");
+
+const mainDivHeight = mainDiv.offsetHeight;
+console.log(mainDivHeight, mainDiv.clientHeight);
+document.documentElement.style.setProperty(
+	"--main-height",
+	`${mainDivHeight}px`
+);
+
+// LEAFLET
 const map = L.map("map").setView([51.505, -0.09], 13);
 L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
 	attribution:
 		'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
 
-const marker = L.marker([51.5, -0.09])
-	.addTo(map)
-	.bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
-	.openPopup();
+const marker = L.marker([51.5, -0.09]).addTo(map);
 console.log(marker);
