0ac2f59df1071e2b4c5e3a5c4ce26128dc629ad1
Bence <m.bence211@gmail.com>
2023-02-23 19:56:57 +0100

    api request

--- main.js
+++ main.js
@@ -10,6 +10,25 @@ document.documentElement.style.setProperty(
 	`${mainDivHeight}px`
 );
 
+//GETTING THE IP
+const main = async function () {
+	try {
+		const res = await fetch(
+			`https://geo.ipify.org/api/v2/country,city?apiKey=${process.env.GEO_KEY}&ipAddress=8.8.8.8`
+		);
+		const data = await res.json();
+		console.log(data);
+
+		if (!res.ok) {
+			console.log(data.description);
+			return;
+		}
+		return data;
+	} catch (error) {
+		console.error(error);
+	}
+};
+
 // LEAFLET
 const map = L.map("map").setView([51.505, -0.09], 13);
 L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
@@ -19,3 +38,8 @@ L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
 
 const marker = L.marker([51.5, -0.09]).addTo(map);
 console.log(marker);
+
+(async function () {
+	const response = await main();
+	console.log(response);
+})();
